c28e214894d039a215e913f7cef1243a713abeff
karan <chickkaran@gmail.com>
2025-01-19 00:19:11 -0800

    Add popup components

---
+++ frontend/components/popups/DefenderGameoverMsg.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function defenderGameover() {
+    return (
+        <div className="bg-gray-200 p-6 rounded-lg text-center">
+            <p className="text-gray-800 font-bold">Message from Pete Polar(boss): Come see me in my office NOW</p>
+        </div>
+    );
+}
+
+export default defenderGameover;

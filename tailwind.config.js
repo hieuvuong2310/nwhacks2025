c33deba55f5459ddccf305ac0db5c084ba24353e
yood2 <yood2@proton.me>
2025-01-18 17:02:45 -0800

    make a game and send code

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // ... existing code ...
+    theme: {
+        extend: {
+            fontFamily: {
+                jersey: ['var(--font-jersey)'],
+            },
+        },
+    },
+    // ... existing code ...
+};

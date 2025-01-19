cfd3ab9a1c00ec67e646a8b46c656ceb53a4ff9e
Trong Hieu Vuong <hieuvg02@gmail.com>
2025-01-18 20:35:33 -0800

    add computer

---
+++ frontend/app/computer/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ComputerPage() {
+    return (
+        <div className='flex justify-center items-center'>
+            <div className='flex flex-col items-center justify-center'>
+                <img src="https://firebasestorage.googleapis.com/v0/b/personal-web-4022f.firebasestorage.app/o/computer_trans.png?alt=media&token=9f41219a-1d3b-4f56-856e-80b64bfe46bc" alt='computer' width={1000}/>
+            </div>
+        </div>
+    );
+};
+
+export default ComputerPage;
